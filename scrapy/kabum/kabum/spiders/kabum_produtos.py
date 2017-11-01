02666d0abff78d6f2f3ed7e3cb73d185c36b95bc
Luiz Braga <luizbragaa@gmail.com>
2017-11-01 16:44:13 -0300

    Adicionando exemplo de selenium

--- scrapy/kabum/kabum/spiders/kabum_produtos.py
+++ scrapy/kabum/kabum/spiders/kabum_produtos.py
@@ -5,7 +5,7 @@ import scrapy
 class KabumProdutosSpider(scrapy.Spider):
     name = 'kabum-produtos'
     allowed_domains = ['https://www.kabum.com.br/']
-    start_urls = ['http://https://www.kabum.com.br//']
+    start_urls = ['https://www.kabum.com.br/']
 
     def parse(self, response):
         pass
--- scrapy/portal_transparencia/portal_transparencia/spiders/portal.py
+++ scrapy/portal_transparencia/portal_transparencia/spiders/portal.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from selenium import webdriver
 
 
 class PortalSpider(scrapy.Spider):
     name = 'portal'
     allowed_domains = ['http://www.portaltransparencia.gov.br/']
-    start_urls = ['http://http://www.portaltransparencia.gov.br//']
+    start_urls = ['http://www.portaltransparencia.gov.br/']
+
+    def __init__(self, *args, **kwargs):
+        super(PortalSpider, self).__init__(*args, **kwargs)
+        self.driver = webdriver.PhantomJS()
+        self.driver.set_window_size(800, 600)
 
     def parse(self, response):
         pass
